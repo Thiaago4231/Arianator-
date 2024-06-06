cfd223f9fa387d99bc98ac777402394df7526b34
Thiaago4231 <116652808+Thiaago4231@users.noreply.github.com>
2024-06-05 21:47:10 -0300

    main.js

---
+++ main.js
@@ -0,0 +1,25 @@
+const caixaPrincipal = document.querySelector(“.caixa-
+principal”);
+
+const caixaPerguntas = document.querySelector(“.caixa-
+perguntas”);
+
+const caixaAlternativas = document.querySelector(“.caixa-
+alternativas”);
+
+const caixaResultado = document.querySelector(“.caixa-
+resultado”);
+
+const textoResultado = document.querySelector(“.texto-
+resultado”);
+
+const perguntas = [
+{
+enunciado: “Quantos Grammy a cantora Taylor Swift - indústria da música - conquistou até o momento?”,
+alternativas: [“09 Grammy”, “14 Grammy”],
+},
+{
+enunciado: “Quantos álbuns a Ilustre Loirinha, Taylor Swift, possui até esse ano?”,
+alternativas: [“07 álbuns 1”, “11 ábuns”],
+},
+];
